63ff0f627e636a758c3c747ae258adb4c4031878
Edward <edward.a56@yahoo.com>
2020-07-14 00:40:44 +0000

    update

--- foodapp_project/settings.py
+++ foodapp_project/settings.py
@@ -38,11 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third-party
+    'crispy_forms',
+
     # Local Apps
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 
 ]
+
+# django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
